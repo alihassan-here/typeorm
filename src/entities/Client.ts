512a93da1765c924001a75c680d22ab863a73a5f
Ali Hassan <alihassanuog50.com>
2022-08-11 17:16:53 +0500

    inheritance

--- src/entities/Client.ts
+++ src/entities/Client.ts
@@ -1,33 +1,10 @@
-import { Entity, BaseEntity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Entity, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Person } from "./utils/Person";
 
 
 
 @Entity("client")
-export class Client extends BaseEntity {
-    @PrimaryColumn()
-    id: number
-
-    @Column()
-    first_name: string;
-
-    @Column()
-    last_name: string;
-
-    @Column({
-        unique: true
-    })
-    email: string
-
-    @Column({
-        unique: true,
-        length: 10
-    })
-    card_number: string
-
-    @Column({
-        type: 'numeric'
-    })
-    balance: number
+export class Client extends Person {
 
     @Column({
         default: true,
